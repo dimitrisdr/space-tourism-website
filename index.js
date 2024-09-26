5712d9dc0668773665d05849e0fb342db8226e60
Dimitris_Dr <jidrist18@hotmail.com>
2024-09-26 13:14:27 +0300

    added functionality to close btn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ let nav = document.querySelector('.navbar')
 
 menuBtn.addEventListener('click', ()=> {
     let isNavExpanded = nav.getAttribute('aria-expanded') === 'true' ? 'false':'true'
-    nav.setAttribute('aria-expanded', isNavExpanded.toString())
+    nav.setAttribute('aria-expanded', isNavExpanded)
+    menuBtn.setAttribute('data-opened', isNavExpanded)
 })
